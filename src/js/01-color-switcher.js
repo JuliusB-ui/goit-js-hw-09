82aff9a131c340a8f2d36755ea1eeae6bb446f72
JuliusB-ui <baraquiel.julius@gmail.com>
2024-05-07 08:33:28 +0800

    01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const body = document.querySelector('body');
+let startBtn = document.querySelector('[data-start]');
+let stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+function startColorChange() {
+  colorChangeInterval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+}
+
+function stopColorChange() {
+  clearInterval(colorChangeInterval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
---
+++ src/js/02-timer.js
@@ -0,0 +1,3 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
